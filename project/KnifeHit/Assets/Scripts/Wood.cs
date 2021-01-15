a00d84a0344a931a8d6ab97f35604ed6a1244f21
reg00 <vgrte@mail.ru>
2021-01-15 13:10:42 +0500

    no message

---
+++ project/KnifeHit/Assets/Scripts/Wood.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Wood : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, 0, speed);
+    }
+}
